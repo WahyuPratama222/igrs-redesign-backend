5009e24bd0fadbfe2322103b08f57e4cb90b9cc6
Wahyu Pratama <wahyupratama110107@gmail.com>
2026-07-03 02:03:33 +0700

    chore(main): add helmet security middleware

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
