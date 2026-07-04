0b5e8129e960c38b70c56e30fd7aa35c6b6afa88
Wahyu Pratama <wahyupratama110107@gmail.com>
2026-07-04 18:06:44 +0700

    chore(main): add cors configuration

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET'],
+    credentials: false,
+  });
+
   app.use(helmet());
 
   app.useGlobalPipes(new ValidationPipe({
